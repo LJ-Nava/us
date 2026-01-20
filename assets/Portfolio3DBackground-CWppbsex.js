fc91de797f4cf8ae63a576093a54a5f05b218a4a
LJ-Nava <developerluis17@gmail.com>
2026-01-20 10:07:44 -0500

    Updates

--- assets/Portfolio3DBackground-CWppbsex.js
+++ assets/Portfolio3DBackground-CWppbsex.js
@@ -1,4 +1,4 @@
-import{r as i,j as e}from"./index-DhiVLRnQ.js";import{u as h,C as R,a as C}from"./react-three-fiber.esm-DKTkACG0.js";import{R as z,C as y,A as w,V as E,S as F,a as P}from"./three.module-BOqUkN66.js";import{F as I}from"./Float-COblXbhb.js";const _=()=>parseInt(z.replace(/\D+/g,"")),L=_();class T extends P{constructor(){super({uniforms:{time:{value:0},fade:{value:1}},vertexShader:`
+import{r as i,j as e}from"./index-B5StK65g.js";import{u as h,C as R,a as C}from"./react-three-fiber.esm-D2mIODcc.js";import{R as z,C as y,A as w,V as E,S as F,a as P}from"./three.module-BOqUkN66.js";import{F as I}from"./Float-L-cz3vvQ.js";const _=()=>parseInt(z.replace(/\D+/g,"")),L=_();class T extends P{constructor(){super({uniforms:{time:{value:0},fade:{value:1}},vertexShader:`
       uniform float time;
       attribute float size;
       varying vec3 vColor;
