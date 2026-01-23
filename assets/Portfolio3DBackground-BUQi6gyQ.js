86851db17dfcc8350543f2efd36fbd426a55aba2
LJ-Nava <developerluis17@gmail.com>
2026-01-23 17:07:30 -0500

    Updates

--- assets/Portfolio3DBackground-BUQi6gyQ.js
+++ assets/Portfolio3DBackground-BUQi6gyQ.js
@@ -1,4 +1,4 @@
-import{r as i,j as e}from"./index-CsXVt5wT.js";import{u as h,C as R,a as C}from"./react-three-fiber.esm-EiE3xDGb.js";import{R as z,C as y,A as w,V as E,S as F,a as P}from"./three.module-BOqUkN66.js";import{F as I}from"./Float-Dwk6rFv6.js";const _=()=>parseInt(z.replace(/\D+/g,"")),L=_();class T extends P{constructor(){super({uniforms:{time:{value:0},fade:{value:1}},vertexShader:`
+import{r as i,j as e}from"./index-C1CewtEP.js";import{u as h,C as R,a as C}from"./react-three-fiber.esm-Cq32ExHH.js";import{R as z,C as y,A as w,V as E,S as F,a as P}from"./three.module-BOqUkN66.js";import{F as I}from"./Float-DD9VrvK1.js";const _=()=>parseInt(z.replace(/\D+/g,"")),L=_();class T extends P{constructor(){super({uniforms:{time:{value:0},fade:{value:1}},vertexShader:`
       uniform float time;
       attribute float size;
       varying vec3 vColor;
